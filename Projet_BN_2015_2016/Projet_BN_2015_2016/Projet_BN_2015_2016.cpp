b1d10e009d057cd65bf192f8693696bd5a41bea7
Briffod Valentin <valentin.briffod@etu.univ-savoie.fr>
2015-09-17 23:22:43 -0400

    test hello world - I assume , plz don't troll - stagiaire en formation

--- Projet_BN_2015_2016/Projet_BN_2015_2016/Projet_BN_2015_2016.cpp
+++ Projet_BN_2015_2016/Projet_BN_2015_2016/Projet_BN_2015_2016.cpp
@@ -1,11 +1,16 @@
 // Projet_BN_2015_2016.cpp : définit le point d'entrée pour l'application console.
 //
 
+#include <iostream>
 #include "stdafx.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	char i;
+
+	std::cout << "hello world _ bribri" << std::endl;
+	std::cin >> i;
 	return 0;
 }
 
