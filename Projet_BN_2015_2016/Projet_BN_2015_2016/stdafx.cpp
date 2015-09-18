e6d5094661cd2832eccba1005660352f8336fae0
Wolfsimjo <sloeb-jeje@hotmail.fr>
2015-09-17 22:43:54 -0400

    First commit create project in visual studio 2013

---
+++ Projet_BN_2015_2016/Projet_BN_2015_2016/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : fichier source incluant simplement les fichiers Include standard
+// Projet_BN_2015_2016.pch représente l'en-tête précompilé
+// stdafx.obj contient les informations de type précompilées
+
+#include "stdafx.h"
+
+// TODO: faites référence aux en-têtes supplémentaires nécessaires dans STDAFX.H
+// absents de ce fichier
